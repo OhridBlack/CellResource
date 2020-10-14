d7b3c60998fd714185272844f9c4995dd5363ecf
OhridBlack <37727776+OhridBlack@users.noreply.github.com>
2020-10-14 21:03:50 +0800

    python code to calcuate curvature

---
+++ LineCurvature2D.py
@@ -0,0 +1,80 @@
+import numpy as np
+import math
+def length(a,b):
+    return math.sqrt((a[0]-b[0])**2+(a[1]-b[1])**2)
+'''
+def inv3(m):
+    adjm=np.array([3,3])
+    adjm[0][0]=m[1][1]*m[2][2]-m[2][1]*m[1][2]
+    adjm[0][1]=-(m[1][0]*m[2][2]-m[2][0]*m[1][2])
+    adjm[0][2]=m[1][0]*m[2][1]-m[2][0]*m[1][1]
+    adjm[1][0]=-(m[0][1]*m[2][2]-m[2][1]*m[0][2])
+    adjm[1][1]=m[0][0]*m[2][2]-m[2][0]*m[0][2]
+    adjm[1][2]=-(m[0][0]*m[2][1]-m[2][0]*m[0][1])
+    adjm[]
+'''
+def lineCurvature2D(Vertces,Lines):
+    '''
+    :param Vertices: 共M个点，M*2维【x，y】
+    :param Lines: 共N条折线，N*2维【左点，右点】
+    :return: M个曲率
+    x=a3 t^2+ a2 t+ a1
+    y=b3 t^2+ b2 t+ b1
+    利用本身点和其前后相邻的两点，拟合以上曲线，并求曲率
+    '''
+    vertices=np.array(Vertces)
+    Na=-np.ones((vertices.shape[0],1))
+    Nb=-np.ones((vertices.shape[0],1))
+    for line in Lines:
+        Na[line[0]]=line[1]
+        Nb[line[1]]=line[2]
+    for i in range(Na.shape[0]):
+        if Na[i]==-1:
+            Na[i]=i
+        if Nb[i]==-1:
+            Nb[i]=i
+    Disa=np.zeros((vertices.shape[0],1))
+    Disb=np.zeros((vertices.shape[0],1))
+    for i in range(Na.shape[0]):
+        Disa[i]=-length(vertices[i],vertices[Na[i]])
+        Disb[i]=length(vertices[i],vertices[Nb[i]])
+
+    x=np.zeros([vertices.shape[0],3])
+    y=np.zeros([vertices.shape[0],3])
+    for i in range(vertices.shape[0]):
+        x[i][0]=vertices[Na[i]][0]
+        x[i][1] = vertices[i][0]
+        x[i][2] = vertices[Nb[i]][0]
+        y[i][0] = vertices[Na[i]][1]
+        y[i][1] = vertices[i][1]
+        y[i][2] = vertices[Nb[i]][1]
+
+    M=np.zeros([vertices.shape[0],3,3])
+    for i in range(M.shape[0]):
+        M[i][0][0]=1
+        M[i][0][1]=-Disa[i]
+        M[i][0][2]=Disa[i]**2#1 -disa disa**2
+        M[i][1][0]=1##100
+        M[i][2][0]=1
+        M[i][2][1]=-Disb[i]
+        M[i][2][2]=Disb[i]**2#1 -disb disb**2
+    '''
+        x=a3 t^2+ a2 t+ a1
+        y=b3 t^2+ b2 t+ b1
+        k=2(a2*b3-a3*b2)/((b2**2+a2**2)**(3/2))
+        '''
+    k=np.zeros((vertices.shape[0],1))
+    for i in range(vertices.shape[0]):
+        invm=np.linalg.inv(M[i])
+        a2=invm[0][1]*x[i][0]+invm[1][1]*x[i][1]+invm[2][1]*x[i][2]
+        a3=invm[0][2]*x[i][0]+invm[1][2]*x[i][1]+invm[2][2]*x[i][2]
+        b2 = invm[0][1] * y[i][0] + invm[1][1] * y[i][1] + invm[2][1] * y[i][2]
+        b3 = invm[0][2] * y[i][0] + invm[1][2] * y[i][1] + invm[2][2] * y[i][2]
+        k[i]=2*(a2*b3-a3*b2)/((a2**2+b2**2)**(3/2))
+
+    return k
+
+
+
+
+
