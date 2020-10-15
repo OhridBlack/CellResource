757d0125ba8231be06c78001cd01d87a69b87ee1
OhridBlack <37727776+OhridBlack@users.noreply.github.com>
2020-10-15 20:14:49 +0800

    add new plots

--- data_plot.py
+++ data_plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import pandas as pd
 
 def violin_plot(data,features_name="curvature",labels=["1","2","3","4"]):
     fig,axes=plt.subplots(figsize=(12,5))
@@ -25,10 +26,38 @@ def density2D_plot(feature_x,feature_y,plot_name,feature_x_name,feature_y_name):
 #density2D_plot(x,y,"plot_name","feature_x","feature_y")
 
 def scatter2D_plot(data,plot_name,feature_x_name,feature_y_name):
-    sns.lmplot(x=feature_x_name,y=feature_y_name,data=data,scatter=True)
+    #sns.lmplot(x=feature_x_name,y=feature_y_name,data=data,scatter=True)
+    data_plot=pd.DataFrame({feature_x_name:data[:,0],feature_y_name:data[:,1]})
+    sns.scatterplot(x=feature_x_name,y=feature_y_name,data=data_plot)
     plt.title(plot_name)
     plt.show()
 
 #data=[[5*i,i*i]for i in range(100)]
 #data=np.array(data)
 #scatter2D_plot(data,"plot_name","feature_x","feature_y")
+
+def scatter_density_plot(data,plot_name,spec_feature,features_name):
+    #data_plot=pd.DataFrame({features_name[0]:data[:,0],features_name[1]:data[:,1]})
+    sns.pairplot(data, vars=features_name,
+                 kind='scatter', diag_kind='kde',
+                 hue=spec_feature, palette='husl')
+    plt.title(plot_name)
+    plt.show()
+
+#iris=sns.load_dataset("iris")
+#scatter_density_plot(iris,"iris","species",['sepal_width', 'sepal_length'])
+#print(iris)
+
+def scatter2D_background_plot(data,plot_name,feature_x_name,feature_y_name,background_path):
+    background=plt.imread(background_path)
+    plt.imshow(background)
+    plt.scatter(data[:,0],data[:,1])
+    plt.title(plot_name)
+    plt.xlabel(feature_x_name)
+    plt.ylabel(feature_y_name)
+    plt.show()
+
+#data=[[i,i*0.5+i*i*0.1]for i in range(100)]
+#data=np.array(data)
+#scatter2D_background_plot(data,"pn","x","y","C:\\Users\\Black\\Desktop\\maomao.jpg")
+
