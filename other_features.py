8559968019b9d892b14d4de06f2f62567564be0a
OhridBlack <37727776+OhridBlack@users.noreply.github.com>
2020-10-19 19:46:20 +0800

    Update other_features.py

--- other_features.py
+++ other_features.py
@@ -2,6 +2,7 @@ import numpy as np
 from readSWC import NT
 from readSWC import readSWC_NT
 from projectSWC import project2D
+from angle_hist import caculateAngle_Hist
 '''
 other features:
 1、 area ：after projecting the 3d swc points to 2d, the area of region
@@ -11,18 +12,20 @@ other features:
 5、 follow x/y: the angle between the normal of projected plane and X-axis or Y-axis
 '''
 
-def area(NT):
+def area_angleHist(NT,somaIdx=0):
     co_list=NT.coordinate_list()
-    co_list_projected=project2D(co_list,0,True)
+    co_list_projected=project2D(co_list,somaIdx)
     x_min=np.min(co_list_projected[:,0])
     x_max=np.max(co_list_projected[:,0])
     y_min = np.min(co_list_projected[:, 1])
     y_max = np.max(co_list_projected[:, 1])
     area=(x_max-x_min)*(y_max-y_min)
-    return area,[[x_min,y_min],[x_max,y_max]]
+    angle_hist=caculateAngle_Hist(co_list_projected)
+
+    return area,[[x_min,y_min],[x_max,y_max]],angle_hist
 
 def other_features(NT):
-    a,rect=area(NT)
+    a,rect,angle_hist=area_angleHist(NT)
     branch_number=len(NT.branch_list)
     tip_number=len(NT.tip_list)
     total_number=len(NT.swc_list)
@@ -34,11 +37,13 @@ def other_features(NT):
     print("branch_density:",branch_density)
     print("tip_density:",tip_density)
     print("total_density:",total_density)
+    print("angle_hist",angle_hist)
+    return a,branch_density,tip_density,total_density,angle_hist
 
-nt=readSWC_NT("C:\\Users\\Black\\Desktop\\r10_with_autoarbor_m1\\r10_17302_00001.swc")
-nt.count_tip_branch()
-print("total numbers:",len(nt.swc_list))
-print("tip numbers:",len(nt.tip_list))
-print("branch numbers:",len(nt.branch_list))
-print("start numbers:",len(nt.start_list))
-other_features(nt)
+#nt=readSWC_NT("C:\\Users\\Black\\Desktop\\r10_with_autoarbor_m1\\r10_17302_00001.swc")
+#nt.count_tip_branch()
+#print("total numbers:",len(nt.swc_list))
+#print("tip numbers:",len(nt.tip_list))
+#print("branch numbers:",len(nt.branch_list))
+#print("start numbers:",len(nt.start_list))
+#other_features(nt)
