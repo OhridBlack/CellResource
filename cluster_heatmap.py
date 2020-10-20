6031a413d31b5fbe4c12fda31b9170c52f899bbb
OhridBlack <37727776+OhridBlack@users.noreply.github.com>
2020-10-20 21:39:59 +0800

    Update cluster_heatmap.py

--- cluster_heatmap.py
+++ cluster_heatmap.py
@@ -14,13 +14,27 @@ from scipy.cluster import hierarchy
 #sns.heatmap(newdf.T)
 #plt.show()
 
-def hierarchy_cluster(data_cluster):
-    z=hierarchy.linkage(data_cluster)
-    hierarchy.dendrogram(z)
+def hierarchy_cluster(data_cluster,method="single",labels=None,plt_name=None):
+    z=hierarchy.linkage(data_cluster,method=method)
+    #print(z)
+    hierarchy.dendrogram(z,labels=labels)
+    '''labels_cut=hierarchy.cut_tree(z,height=0.45)
+    #print(labels_cut)
+    dic={}
+    for label_cut in labels_cut:
+        if label_cut[0] not in dic:
+            dic.update({label_cut[0]:1})
+        else:
+            dic[label_cut[0]]+=1
+    print(dic)
+    '''
+    if plt_name:
+        plt.title(plt_name)
     plt.show()
+    return z
 
 #print(newdf.index)
-#hierarchy_cluster(newdf)
+#hierarchy_cluster(newdf,newdf.index)
 
 def heatmap(data_heatmap):
     sns.heatmap(data_heatmap.T)
